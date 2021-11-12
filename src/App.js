a3bc2fbfe46156aa4e7df55526616170bf7756e4
farhan-begg <farhan.begg@students.makeschool.com>
2021-11-11 23:16:27 -0800

    implemented login and register page frontend only

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
 const App = () => {
     return (
         <div>
-           <Home />
+           <Login />
         </div>
     )
 }
